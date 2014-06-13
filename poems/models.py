9088f415cd9e5236e02d930b6c146d89993b11cc
Antonio Campos <acamposruiz@gmail.com>
2014-06-13 20:45:06 +0200

    Creando libreria slughifi

--- poems/models.py
+++ poems/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+# from easy_thumbnails.fields import ThumbnailerImageField
 
-# Create your models here.
+class Poem(models.Model):
+
+	title = models.CharField(max_length=60)
+	body = models.TextField()
+	user = models.ForeignKey('userprofile.User', blank=True, null=True, on_delete=models.SET_NULL)
+	users = models.ManyToManyField('userprofile.User', blank=True, null=True, on_delete=models.SET_NULL)
+	tags = models.ManyToManyField('tags.Tag', blank=True, null=True, on_delete=models.SET_NULL)
+	created = models.DateTimeField(auto_now_add=True)
+	update = models.DateTimeField(auto_now=True)
+
+	def __unicode__(self):
+		return self.title
