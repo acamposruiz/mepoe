a4405e636e3a7e5fa5da4aeef632c36c3910ab65
Antonio Campos <acamposruiz@gmail.com>
2014-06-13 22:52:02 +0200

    Trabajando en los modelos de tags

--- poems/models.py
+++ poems/models.py
@@ -1,15 +1,23 @@
 from django.db import models
+from libs.slughifi import slughifi
 # from easy_thumbnails.fields import ThumbnailerImageField
 
 class Poem(models.Model):
 
-	title = models.CharField(max_length=60)
+	title = models.CharField(max_length=60, blank=True, null=True)
 	body = models.TextField()
 	user = models.ForeignKey('userprofile.User', blank=True, null=True, on_delete=models.SET_NULL)
 	users = models.ManyToManyField('userprofile.User', blank=True, null=True, on_delete=models.SET_NULL)
 	tags = models.ManyToManyField('tags.Tag', blank=True, null=True, on_delete=models.SET_NULL)
+	slug = models.SlugField(max_length=100)
 	created = models.DateTimeField(auto_now_add=True)
 	update = models.DateTimeField(auto_now=True)
 
 	def __unicode__(self):
 		return self.title
+
+	def save(self, *args, **kwargs):
+		if not self.title:
+	        self.title = self.body[:60]
+	    self.slug = slughifi(self.title)
+	    super(Poem, self).save(*args, **kwargs)
---
+++ tags/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from libs.slughifi import slughifi
+# from easy_thumbnails.fields import ThumbnailerImageField
+
+class Tag(models.Model):
+
+	name = models.CharField(max_length=60)
+	description = models.TextField(blank=True, null=True)
+	slug = models.SlugField(max_length=100)
+	parent = models.ForeignKey('self', blank=True, null=True, on_delete=models.SET_NULL)
+	vid = models.ForeignKey('Vocabulary', blank=True, null=True, on_delete=models.SET_NULL)
+	created = models.DateTimeField(auto_now_add=True)
+	update = models.DateTimeField(auto_now=True)
+
+	def __unicode__(self):
+		return self.name
+
+	def save(self, *args, **kwargs):
+	    self.slug = slughifi(self.name)
+	    super(Tag, self).save(*args, **kwargs)
+
+
+class Vocabulary(models.Model):
+
+	name = models.CharField(max_length=60)
+	description = models.TextField(blank=True, null=True)
+	slug = models.SlugField(max_length=100)
+	created = models.DateTimeField(auto_now_add=True)
+	update = models.DateTimeField(auto_now=True)
+
+	def __unicode__(self):
+		return self.name
+
+	def save(self, *args, **kwargs):
+	    self.slug = slughifi(self.name)
+	    super(Vocabulary, self).save(*args, **kwargs)
