6f96624eb9ab762128dc260cd5b11d975f205adf
Antonio Campos <acamposruiz@gmail.com>
2014-07-18 14:12:23 +0200

    borrando antiguo module userprofile

--- userprofiles/urls.py
+++
@@ -1,8 +0,0 @@
-# urls.py
-from django.conf.urls import patterns, url
-from .views import *
-
-urlpatterns = patterns('',
-    url(r'signup/$', signup, name='signup'),
-    url(r'signin/$', signin, name='signin'),
-)
