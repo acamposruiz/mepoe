8654073ea94ced450f23bd0bf2a480a19c852596
Antonio Campos <acamposruiz@gmail.com>
2014-09-09 23:06:29 +0200

    Trabajando en los templates

--- poems/views.py
+++ poems/views.py
@@ -24,6 +24,20 @@ class PoemList(ListView):
     model = Poem
     queryset = Poem.objects.all().order_by('-pub_date')[0:3]
 
+    def get_context_data(self, **kwargs):
+            # Call the base implementation first to get a context
+            context = super(PoemList, self).get_context_data(**kwargs)
+            # Add in a QuerySet of all the books
+            context['front'] = True
+            context['content'] = {}
+            context['content']['text'] = {
+                'head': {
+                    'title': 'Last Works',
+                    'body':
+                    'Collaborative production of poetry and short stories'
+                }}
+            return context
+
 
 class PoemDetail(DetailView):
     model = Poem
